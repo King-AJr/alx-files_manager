56be4a0ac81ea843da2fbeccfb2969360d4d0151
YemiDev <abodunrinsulaimon1@gmail.com>
2023-09-30 16:05:28 +0100

    Version Update

---
+++ utils/db.js
@@ -0,0 +1,34 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    this.client = new MongoClient(`mongodb://${host}:${port}/${database}`);
+    this.client.on('error', (err) => {
+      console.log(`MongoDB connection client error: ${err}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const collection = this.client.db().collection('users');
+    const count = await collection.countDocuments();
+    return count;
+  }
+
+  async nbFiles() {
+    const collection = this.client.db().collection('files');
+    const count = await collection.countDocuments();
+    return count;
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
