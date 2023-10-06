ce7e864c78ce411eec0b1be29adf34e17c9a1980
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-10-06 10:42:19 +0100

    .

--- Automation-Files/redis_installation.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-# A program file that installs Redis
-
-wget http://download.redis.io/releases/redis-6.0.10.tar.gz
-
-tar xzf redis-6.0.10.tar.gz
-
-cd redis-6.0.10
-make
--- Automation-Files/systeminfo.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-# Display system information and Version
-cat /etc/lsb-release
